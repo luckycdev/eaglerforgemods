9ba062baebdb37c85b4183fa3b115ae85d460e38
Vincent <luckzstuff@gmail.com>
2024-03-13 21:14:13 -0500

    add togglesprint

---
+++ togglesprint.js
@@ -0,0 +1,20 @@
+let sprintToggled = false;
+function toggleSprint(enable) {
+    sprintToggled = enable;
+    ModAPI.player.serverSprintState = sprintToggled;
+    ModAPI.player.reload();
+    ModAPI.displayToChat({ msg: sprintToggled ? "Sprint Toggled ON" : "Sprint Toggled OFF" });
+}
+ModAPI.addEventListener("sendchatmessage", event => {
+    const message = event.message.toLowerCase().trim();
+    if (message === ".togglesprint on") {
+        toggleSprint(true);
+        event.preventDefault = true;
+    } else if (message === ".togglesprint off") {
+        toggleSprint(false);
+        event.preventDefault = true;
+    } else if (message === ".togglesprint") {
+        ModAPI.displayToChat({ msg: "Usage: .togglesprint on or .togglesprint off" });
+        event.preventDefault = true;
+    }
+});
