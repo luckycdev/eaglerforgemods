be8bca6d21c40686a9e87a0cfbe367d1453c9653
Vincent <luckzstuff@gmail.com>
2024-04-04 15:57:24 -0500

    not working

--- togglesprint.js
+++ togglesprint.js
@@ -1,5 +1,55 @@
-ModAPI.require("network");
+ModAPI.addEventListener("update", function (){
+    if(sprintToggled){
+            if(ModAPI.player.moveForward != 0 && !ModAPI.player.isSneaking() && !ModAPI.player.isBlocking()){ //TODO
+                ModAPI.player.setSprinting({flag: true})
+                ModAPI.player.reload()
+            } else {
+                ModAPI.player.setSprinting({flag: false})
+                ModAPI.player.reload()
+            }
+    } else {
+            ModAPI.player.setSprinting({flag: false})
+            ModAPI.player.reload()
+    }
+    
+})
 
-ModAPI.addEventListener("update", ()=>{
-    ModAPI.network.sendPacketEntityAction({entityId: 0, action: START_SPRINTING, auxData: Integer});
+
+ModAPI.require("player");
+let sprintToggled = false;
+function updateSprintState() {
+    if (sprintToggled) {
+        ModAPI.player.setSprinting({flag: true});
+    } else {
+        ModAPI.player.setSprinting({flag: false});
+    }
+    ModAPI.player.reload();
 }
+
+function toggleSprintOn() {
+    sprintToggled = true;
+    if(ModAPI.player.moveForward != 0 && !ModAPI.player.isSneaking() && !ModAPI.player.isBlocking()){
+    updateSprintState();
+    ModAPI.displayToChat({ msg: "ToggleSprint activated." });
+}
+}
+
+function toggleSprintOff() {
+    sprintToggled = false;
+    updateSprintState();
+    ModAPI.displayToChat({ msg: "ToggleSprint deactivated." });
+}
+
+ModAPI.addEventListener("key", function(ev){
+    if(ev.key == 34){
+        if(sprintToggled){
+            sprintToggled = false
+        } else {
+            sprintToggled = true
+        }
+    }
+})
+
+/*ModAPI.addEventListener("update", () => {
+    updateSprintState();
+});*/
