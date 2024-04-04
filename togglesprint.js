28eb4027e710585679b04740f54e820635b0d918
lucky <50680260+luckycdev@users.noreply.github.com>
2024-04-04 09:16:01 -0500

    wip not working

--- togglesprint.js
+++ togglesprint.js
@@ -1,35 +1,5 @@
-ModAPI.require("player");
-let sprintToggled = false;
-function updateSprintState() {
-    if (sprintToggled) {
-        ModAPI.player.serverSprintState = true;
-    } else {
-        ModAPI.player.serverSprintState = false;
-    }
+ModAPI.require("network");
+
+ModAPI.addEventListener("update", ()=>{
+    ModAPI.network.sendPacketEntityAction({entityId: 0, action: START_SPRINTING, auxData: Integer});
 }
-function toggleSprintOn() {
-    sprintToggled = true;
-    updateSprintState();
-    ModAPI.displayToChat({ msg: "ToggleSprint activated." });
-}
-function toggleSprintOff() {
-    sprintToggled = false;
-    updateSprintState();
-    ModAPI.displayToChat({ msg: "ToggleSprint deactivated." });
-}
-ModAPI.addEventListener("sendchatmessage", (ev) => {
-    const message = ev.message.toLowerCase().trim();
-    if (message === ".togglesprint on") {
-        ev.preventDefault = true;
-        toggleSprintOn();
-    } else if (message === ".togglesprint off") {
-        ev.preventDefault = true;
-        toggleSprintOff();
-    } else if (message === ".togglesprint") {
-        ev.preventDefault = true;
-        ModAPI.displayToChat({ msg: "Incorrect Usage: Use .togglesprint on or .togglesprint off" });
-    }
-});
-ModAPI.addEventListener("update", () => {
-    updateSprintState();
-});
